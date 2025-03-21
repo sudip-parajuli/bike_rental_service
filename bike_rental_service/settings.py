162930447d12e90e6c1d1b62693c74b994594517
sudip-2000 <sparajuli802@gmail.com>
2025-03-21 20:41:10 +0545

    Add requirements, db.json

--- bike_rental_service/settings.py
+++ bike_rental_service/settings.py
@@ -5,6 +5,8 @@ Django settings for bike_rental_service project.
 from pathlib import Path
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -97,6 +99,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
