d1970632096d2295ec0c98ffdf47dda31178b81d
sudip-2000 <sparajuli802@gmail.com>
2026-01-27 12:56:56 +0545

    Fix indentation error in bikes/urls.py

--- bikes/urls.py
+++ bikes/urls.py
@@ -1,3 +1,14 @@
+app_name = 'bikes'
+
+from django.urls import path
+from .views import (
+    BikeListView, BikeDetailView, BikeCreateView, BikeUpdateView, BikeDeleteView,
+    BikeRecommendationsView, SimilarBikesView
+)
+
+urlpatterns = [
+    path('', BikeListView.as_view(), name='bike-list'),
+    path('<int:pk>/', BikeDetailView.as_view(), name='bike-detail'),
     path('create/', BikeCreateView.as_view(), name='bike-create'),
     path('<int:pk>/update/', BikeUpdateView.as_view(), name='bike-update'),
     path('<int:pk>/delete/', BikeDeleteView.as_view(), name='bike-delete'),
