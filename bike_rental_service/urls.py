2ed2be7834166896f1b182251af9e8c5e543f1c7
sudip-2000 <sparajuli802@gmail.com>
2026-01-29 12:48:53 +0545

    Enable media file serving in production (fixes 404 on uploads)

--- bike_rental_service/urls.py
+++ bike_rental_service/urls.py
@@ -55,4 +55,11 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.views.static import serve
+from django.urls import re_path
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Serve media files in production (Workaround for Render without S3/Cloudinary)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
