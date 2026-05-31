8b6cb0320d963635ea5b504e7934cf72fb2c9517
sudip-2000 <sparajuli802@gmail.com>
2026-05-31 22:26:45 +0545

    fix: recommend canonical ping URL and add password settings link to profile

--- bike_rental_service/views.py
+++ bike_rental_service/views.py
@@ -26,7 +26,7 @@ class PingView(View):
     """
     Health-check endpoint for cron jobs / uptime monitors.
     Always returns HTTP 200 with no redirects.
-    Use this URL in cronjob.org: https://easymoto.com.np/ping/
+    Use this URL in cronjob.org: https://www.easymoto.com.np/ping/
     """
     def get(self, request):
         return JsonResponse({
